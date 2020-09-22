14c692df077204a7563aa579486747172c4d04eb
Ailin Chua <ai-chua@outlook.com>
2020-09-22 21:03:20 +0800

    commit prior to pull

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'memories/new'
   get 'memories/create'
   devise_for :users
-  root to: 'memories#new'
+  root to: 'memories#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
