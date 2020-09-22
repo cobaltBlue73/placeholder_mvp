e12129ef21063d8ce15b38fce344fb1d56191051
MuhammadZaid Amir <md.zaid73@gmail.com>
2020-09-22 16:02:50 +0800

    cretated memories controller and relevant veiws

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'memories/index'
+  get 'memories/new'
+  get 'memories/create'
   devise_for :users
-  root to: 'ar_photos#new'
+  root to: 'memories#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
