7e0fecf65d7e7ccddc343e9f5456719638f3d78d
Muhammad_Zaid Amir <md.zaid73@gmail.com>
2020-09-22 08:38:38 +0800

    Proper sizing and positioning of cloudinary image

--- app/controllers/ar_photos_controller.rb
+++ app/controllers/ar_photos_controller.rb
@@ -1,6 +1,6 @@
 class ArPhotosController < ApplicationController
     skip_before_action :authenticate_user!
     def new
-        @poses = %w[b-boy_ynjona singer-blue-pants_cbph5l]
+        @poses = %w[singer-blue-pants_cbph5l.png]
     end
 end
