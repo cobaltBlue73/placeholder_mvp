fc804c689f7bcd69dfc03160b2304b2b7de9efd3
Ailin Chua <ai-chua@outlook.com>
2020-09-23 22:51:31 +0800

    Friend search bar implemented

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   def friends
-    @friends = current_user.friends
+    if params[:query].present?
+      @friends = current_user.friends.where("username ILIKE ?", "%#{params[:query]}%")
+    else
+      @friends = current_user.friends
+    end
   end
 end
