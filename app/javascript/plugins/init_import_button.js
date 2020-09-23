05198a3ed1ddae14a16c1ca2f074c0a8ab995eb3
Daphne Kua <daphnekua@gmail.com>
2020-09-23 19:08:52 +0800

    Done with dynamic counter

--- app/javascript/plugins/init_import_button.js
+++ app/javascript/plugins/init_import_button.js
@@ -1,6 +1,19 @@
 const importButton = () => {
-  console.log("hello");
+  const checkboxes = document.querySelectorAll(".checkbox");
+  const counter = document.querySelector(".import-button .counter");
 
+  checkboxes.forEach((element) => {
+    element.addEventListener('click', () => {
+      let activeCheckboxes = document.querySelectorAll(".checkbox.active").length;
+      if (activeCheckboxes > 0) {
+        counter.classList.add("active")
+        counter.innerHTML = activeCheckboxes;
+      } else {
+        counter.classList.remove("active")
+        counter.innerHTML = "";
+      }
+    })
+  })
 }
 
 export { importButton }
