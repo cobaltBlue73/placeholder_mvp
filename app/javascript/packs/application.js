831e720da048f9a29652a29107d92ae1d4bfe21f
Ailin Chua <ai-chua@outlook.com>
2020-09-23 23:44:05 +0800

    before merge

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -32,6 +32,5 @@ import { resizeAllCards } from '../plugins/init_masonry_layout';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
-
   resizeAllCards();
 });
