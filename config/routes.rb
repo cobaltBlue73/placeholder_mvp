c2131e9032a986e5ec2fd5a5fe7df807e4927cda
Ailin Chua <ai-chua@outlook.com>
2020-09-24 11:21:47 +0800

    added community page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'memories/new'
   get 'memories/create'
   devise_for :users
-  root to: 'memories#index'
+  root to: 'memories#new'
   get 'friends', to: 'users#friends'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
