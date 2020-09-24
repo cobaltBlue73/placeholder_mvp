2cde0d4e72d510091d81b3f2f89ea21d036e1d20
MuhammadZaid Amir <md.zaid73@gmail.com>
2020-09-24 11:54:21 +0800

    Revert "bug fix"
    This reverts commit d5dfed5c6c0144f08f1918e3b85f2c79598c621a.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  get 'memories/index'
+  get 'memories/new'
+  get 'memories/create'
   devise_for :users
-  root to: 'pages#home'
+  root to: 'memories#new'
   get 'friends', to: 'users#friends'
-  get 'camera', to: 'memories#new'
-  get 'memories', to: 'memories#index'
-  post 'memories/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
