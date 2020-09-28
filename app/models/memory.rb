e4c2b629de4fcfffeed8fcc784ddf0d6585bafb1
Ailin Chua <ai-chua@outlook.com>
2020-09-28 17:46:42 +0800

    create memory

--- app/models/memory.rb
+++ app/models/memory.rb
@@ -1,6 +1,6 @@
 class Memory < ApplicationRecord
   belongs_to :user
-  has_many :avatar_memories
+  has_many :avatar_memories, dependent: :destroy
   has_many :avatars, through: :avatar_memories
   has_one_attached :photo, dependent: :destroy
 end
