a4624f16aace73dbf250ab4347c57cf465d727a4
Ailin Chua <ai-chua@outlook.com>
2020-09-28 16:33:12 +0800

    updated import to camera method

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -1,20 +1,29 @@
 class MemoriesController < ApplicationController
   # skip_before_action :authenticate_user!
-
   def index
     @memories = Memory.all
-
   end
 
   def new
-    @avatars = params[:key].split(',');
+    @avatars = params[:key].split(',').map do |id|
+      Avatar.find(id)
+    end
     @avatars = [] if @avatars.nil?
   end
 
   def create
+    @avatars = params[]
+    @memory = Memory.new()
+    @memory.avatars = @avatars
   end
 
   def show
     @memory = Memory.find(params[:id])
+  end
+
+  private
+
+  def memories_params
+    params.require(:memory).permit(:user_id)
   end
 end
