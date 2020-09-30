5d6f3c002f2683d63839fae90b065a4d58c97bb4
Daphne Kua <daphnekua@gmail.com>
2020-09-30 11:36:57 +0800

    Left with testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
   def friends
     if params[:query].present?
-      @friends = current_user.friends.where("username ILIKE ?", "%#{params[:query]}%")
+      @friends = current_user.friends.where("username ILIKE ?", "%#{params[:query]}%").order('username asc')
     else
-      @friends = current_user.friends
+      @friends = current_user.friends.order('username asc')
     end
   end
 end
